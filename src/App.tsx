2b9968f665e3e7a04cdecc64d7ec7a7f6c0cb5e8
Kauan Zajac <kauanzajac@gmail.com>
2023-08-03 10:02:25 -0300

    Instalação de pacotes

--- src/App.tsx
+++ src/App.tsx
@@ -1,64 +1,62 @@
-import { useState, useCallback, useEffect } from 'react'
-import filmes from "./filmes.json"
+import { useState, useCallback, useEffect } from "react";
+import filmes from "./filmes.json";
 // import animais from "./animais.json"
-import HangmanDesenho from "./components/HangmanDesenho"
-import Keyboard from './components/Keyboard'
-import HangmanPalavra from './components/HangmanPalavra'
+import HangmanDesenho from "./components/HangmanDesenho";
+import Keyboard from "./components/Keyboard";
+import HangmanPalavra from "./components/HangmanPalavra";
 
 function App() {
-
   const [palavraParaAcertar, setPalavraParaAcertar] = useState(() => {
-    return filmes[Math.floor(Math.random() * filmes.length)]
-  })
+    return filmes[Math.floor(Math.random() * filmes.length)];
+  });
 
+  const [chuteLetras, setChuteLetras] = useState<string[]>([]);
 
-  const [chuteLetras, setChuteLetras] = useState<string[]>([])
+  const letrasErradas = chuteLetras.filter(
+    (letter) => !palavraParaAcertar.includes(letter)
+  );
 
-  const letrasErradas = chuteLetras.filter(letter => !palavraParaAcertar.includes(letter))
+  const isLoser = letrasErradas.length >= 6;
+  const isWinner = palavraParaAcertar
+    .split("")
+    .every((letter) => chuteLetras.includes(letter));
 
-  const isLoser = letrasErradas.length >= 6
-  const isWinner = palavraParaAcertar.split("").every(letter => chuteLetras.includes(letter))
-
-  const addChuteLetras = useCallback((letter: string) => {
-    if (chuteLetras.includes(letter) || isLoser || isWinner)
-      return
-
-    setChuteLetras(letrasAtuais => [...letrasAtuais, letter])
-  }, [chuteLetras, isWinner, isLoser])
-
-  useEffect(() => {
-
-  })
+  const addChuteLetras = useCallback(
+    (letter: string) => {
+      if (chuteLetras.includes(letter) || isLoser || isWinner)
+        return setChuteLetras((letrasAtuais) => [...letrasAtuais, letter]);
+    },
+    [chuteLetras, isWinner, isLoser]
+  );
 
   useEffect(() => {
     const handler = (e: KeyboardEvent) => {
-      const key = e.key
+      const key = e.key;
 
-      if (!key.match(/^[a-z-]$/)) return
+      if (!key.match(/^[a-z-]$/)) return;
 
-      e.preventDefault()
-      addChuteLetras(key)
-    }
+      e.preventDefault();
+      addChuteLetras(key);
+    };
 
-    document.addEventListener("keypress", handler)
+    document.addEventListener("keypress", handler);
 
     return () => {
-      document.removeEventListener("keypress", handler)
-    }
-  }, [chuteLetras])
+      document.removeEventListener("keypress", handler);
+    };
+  }, [chuteLetras]);
 
   return (
-
-    <div style={{
-      maxWidth: "800px",
-      display: "flex",
-      flexDirection: "column",
-      gap: "2rem",
-      margin: "0 auto",
-      alignItems: "center",
-
-    }}>
-
+    <div
+      style={{
+        maxWidth: "800px",
+        display: "flex",
+        flexDirection: "column",
+        gap: "2rem",
+        margin: "0 auto",
+        alignItems: "center",
+      }}
+    >
       <div style={{ fontSize: "2rem", textAlign: "center" }}>
         {isWinner && "Winner!! - Atualize a pagina para tentar novamente"}
         {isLoser && "Lixo, vc perdeu"}
@@ -69,22 +67,21 @@ function App() {
       <HangmanPalavra
         reveal={isLoser}
         chuteLetras={chuteLetras}
-        palavraParaAcertar={palavraParaAcertar} />
+        palavraParaAcertar={palavraParaAcertar}
+      />
 
-      <div style={{ alignSelf: 'stretch' }}>
+      <div style={{ alignSelf: "stretch" }}>
         <Keyboard
           disabled={isWinner || isLoser}
-          activeLetters={chuteLetras.filter(letter => palavraParaAcertar.includes(letter)
+          activeLetters={chuteLetras.filter((letter) =>
+            palavraParaAcertar.includes(letter)
           )}
           inactiveLetters={letrasErradas}
           addChuteLetra={addChuteLetras}
         />
       </div>
-
-
     </div>
-
-  )
+  );
 }
 
-export default App
+export default App;
